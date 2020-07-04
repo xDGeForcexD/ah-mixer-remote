6058202eea2cbb8c26eb4e0ec3393d031d99acba
xDGeForcexD <info@xdgeforcexd.eu>
2020-07-04 19:13:38 +0200

    methode documentation added

--- lib/drivers/sq/modules/levelToMix.ts
+++ lib/drivers/sq/modules/levelToMix.ts
@@ -12,10 +12,21 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         this.commandBuilder = commandBuilder;
     }
 
+    /**
+     * Takes input number and value to send value on LR Mix to mixer
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     * @param value ValueLevel Class with Level from -inf db to +10db
+     */
     setValue(channel: number, value: ValueLevel) : void {
         this.setValueMix(Mixes.LR, channel, value);
     }
 
+    /**
+     * Takes mix, input number and value to send value to mixer
+     * @param mix Mixes Enum LR, Aux1 - Aux12
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     * @param value ValueLevel Class with Level from -inf db to +10db
+     */
     setValueMix(mix: Mixes, channel: number, value: ValueLevel) : void {
         if(this.communicator === null) {
             throw new Error("no communicator is set");
@@ -30,10 +41,19 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         this.communicator.write(this.commandBuilder.toSendValue(address.msb, address.lsb, valueSend.vc, valueSend.vf));
     };
 
+    /**
+     * Takes a input number to send level increment of 1db on LR Mix to mixer
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     incValue(channel: number) {
         this.incValueMix(Mixes.LR, channel);
     }
 
+    /**
+     * Takes mix and input number to send level increment of 1db to mixer
+     * @param mix Mixes Enum LR, Aux1 - Aux12
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     incValueMix(mix: Mixes, channel: number) {
         if(this.communicator === null) {
             throw new Error("no communicator is set");
@@ -47,10 +67,19 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         this.communicator.write(this.commandBuilder.toSendInc(address.msb, address.lsb));
     }
 
+    /**
+     * Takes a input number to send level decrement of 1db on LR Mix to mixer
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     decValue(channel: number) {
         this.decValueMix(Mixes.LR, channel);
     }
 
+    /**
+     * Takes mix and input number to send level decrement of 1db to mixer
+     * @param mix Mixes Enum LR, Aux1 - Aux12
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     decValueMix(mix: Mixes, channel: number) {
         if(this.communicator === null) {
             throw new Error("no communicator is set");
@@ -64,10 +93,19 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         this.communicator.write(this.commandBuilder.toSendDec(address.msb, address.lsb));
     }
 
+    /**
+     * Takes a input number to request the level on LR Mix from mixer
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     requestValue(channel: number): void {
         this.requestValueMix(Mixes.LR, channel);
     }
 
+    /**
+     * Takes mix and input number to request the level from mixer
+     * @param mix Mixes Enum LR, Aux1 - Aux12
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     requestValueMix(mix: Mixes, channel: number) {
         if(this.communicator === null) {
             throw new Error("no communicator is set");
@@ -80,7 +118,13 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         let address = this.calcAddress(mix, channel);
         this.communicator.write(this.commandBuilder.toGetValue(address.msb, address.lsb));
     }
-
+    
+    /**
+     * Calculates the Address from given mix and channel
+     * @param mix Mixes Enum LR, Aux1 - Aux12
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     * @returns object from msb and lsb
+     */
     private calcAddress(mix: Mixes, channel: number) : {msb: number, lsb: number} {
         let msb : number = 0x40;
         let lsb : number = channel-1;
@@ -97,6 +141,11 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         return {msb: msb, lsb: lsb};
     }
 
+    /**
+     * Check if Channel ist valid (Range 1 - 48)
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     * @returns true for its valid
+     */
     private checkValidChannel(channel: number) : Boolean {
         if(channel > 0 && channel < 49) {
             return true;
@@ -104,6 +153,12 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         return false;
     }
 
+    /**
+     * decode the level from vc and vf
+     * @param vc Value Coarse
+     * @param vf Value Fine
+     * @returns Value in db (Linear Taper)
+     */
     private decode(vc: number, vf: number) : number {
         let gainAs14Bit;
         gainAs14Bit = vc << 7;
@@ -113,6 +168,11 @@ class ModuleSQLevelToMix extends ModuleLevelToMix {
         return +((gainOffB - 0x8000) / 256).toFixed(1);
     }
 
+    /**
+     * encode the level from value in db (Linear Taper)
+     * @param gain Level in db
+     * @returns object with vc and vf
+     */
     private encode(gain: ValueLevel) : {vc: number, vf: number} {
         let gainOffB = (gain.value * 256) + 0x8000;
         let scaledGain = (gainOffB / (35328)) * 0xFFFF;
--- lib/drivers/sq/modules/mute.ts
+++ lib/drivers/sq/modules/mute.ts
@@ -1,5 +1,4 @@
 import ModuleMute from "../../../module/types/mute";
-import IValue from "../../../types/structure/iValue";
 import CommandBuilderSQ from "../commandBuilder";
 import ValueState from "../../../types/structure/valueState";
 
@@ -11,7 +10,12 @@ class ModuleSQMute extends ModuleMute {
         super(commandBuilder);
         this.commandBuilder = commandBuilder;
     }
-    
+
+    /**
+     * Takes input number and value to mute input on mixer
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     * @param value ValueState Class with true / false
+     */
     setValue(channel: number, value: ValueState) : void {
         if(this.communicator === null) {
             throw new Error("no communicator is set");
@@ -25,6 +29,10 @@ class ModuleSQMute extends ModuleMute {
         this.communicator.write(this.commandBuilder.toSendValue(address.msb, address.lsb, 0, value.value ? 1 : 0));
     };
 
+    /**
+     * Takes input number to toggle mute the input on mixer
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     toggleValue(channel: number) : void {
         if(this.communicator === null) {
             throw new Error("no communicator is set");
@@ -38,6 +46,10 @@ class ModuleSQMute extends ModuleMute {
         this.communicator.write(this.commandBuilder.toSendInc(address.msb, address.lsb));
     }
 
+    /**
+     * Takes a input number to request the mute state on the input from mixer
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     */
     requestValue(channel: number): void {
         if(this.communicator === null) {
             throw new Error("no communicator is set");
@@ -51,10 +63,21 @@ class ModuleSQMute extends ModuleMute {
         this.communicator.write(this.commandBuilder.toGetValue(address.msb, address.lsb));
     }
 
+    /**
+     * Calculates the Address from given mix and channel
+     * @param mix Mixes Enum LR, Aux1 - Aux12
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     * @returns object from msb and lsb
+     */
     private calcAddress(channel: number) : {msb: number, lsb: number} {
         return {msb: 0x00, lsb: channel-1};
     }
 
+    /**
+     * Check if Channel ist valid (Range 1 - 48)
+     * @param channel Input number 1 - 48 (e.g. Ip1)
+     * @returns true for its valid
+     */
     private checkValidChannel(channel: number) : Boolean {
         if(channel > 0 && channel < 49) {
             return true;
