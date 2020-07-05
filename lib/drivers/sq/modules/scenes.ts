242b450d2b3bf602df883cca2e607e93437692a4
xDGeForcexD <info@xdgeforcexd.eu>
2020-07-05 13:24:04 +0200

    scene code added

--- lib/drivers/sq/modules/scenes.ts
+++ lib/drivers/sq/modules/scenes.ts
@@ -1,14 +1,44 @@
 import ModuleScenes from "../../../module/types/scenes";
 import IValue from "../../../types/structure/iValue";
+import ValueState from "../../../types/structure/valueState";
 
 class ModuleSQScenes extends ModuleScenes {
     driverRequiere: string = "SQ";
-
-    setValue(channel: number, value: IValue): void {
-        throw new Error("Method not implemented.");
+    
+    /**
+     * Takes scene number and value to change the scene on the mixer
+     * @param channel Scene number 1 - 300
+     * @param value ValueState Class with true / false (only by true the scene is changed)
+     */
+    setValue(channel: number, value: ValueState): void {
+        if(value.value) {
+            this.setScene(channel);
+        }
     }
+    
+    /**
+     * Takes scene number to change the scene on the mixer
+     * @param channel Scene number 1 - 300
+     */
+    setScene(scene: number) {
+        if(this.communicator === null) {
+            throw new Error("no communicator is set");
+        }
+
+        if(scene < 1 || scene > 300) {
+            throw new Error("wrong scene input");
+        }
+
+        this.communicator.write(new Uint8Array([0xB0+this.commandBuilder.midiChannel, 0x00, Math.floor((scene-1) / 0x80), 0xC0, Math.floor((scene-1) % 0x80)]));
+    }
+    
+    /**
+     * Request the actual scene on the mixer
+     * @param channel Scene number 1 - 300
+     * @ignore requestValue option is not available
+     */
     requestValue(channel: number): void {
-        throw new Error("Method not implemented.");
+        throw new Error("option not available");
     }
 
 }
--- test/drivers/sq/modules/testScenes.ts
+++ test/drivers/sq/modules/testScenes.ts
@@ -1,7 +1,112 @@
 import { expect } from "chai";
+import { ImportMock, MockManager } from "ts-mock-imports";
+import * as Communicator from "../../../../lib/communicator/communicator";
+import * as CommandBuilderSQ from "../../../../lib/drivers/sq/commandBuilder";
+import * as DriverSQ from "../../../../lib/drivers/sq/driver";
+import ModuleSQScenes from "../../../../lib/drivers/sq/modules/scenes";
+import Driver from "../../../../lib/driver/driver";
+import ValueState from "../../../../lib/types/structure/valueState";
+
+const sinon = require("sinon");
 
 describe("TestModuleSQScenes", function() {
-    it("implement", function() {
-        expect(false).to.be.true;
+    let communicatorMock : MockManager<Communicator.default>;
+    let commandBuilderMock : MockManager<CommandBuilderSQ.default>;
+    let driverMock : MockManager<Driver>
+    let driver : Driver;
+    let communicator : Communicator.default;
+    let commandBuilder : CommandBuilderSQ.default;
+    let scene : ModuleSQScenes;
+
+    const sandbox = sinon.createSandbox();
+    beforeEach(function() {
+        communicatorMock = ImportMock.mockClass(Communicator);
+        commandBuilderMock = ImportMock.mockClass(CommandBuilderSQ);
+        driverMock = ImportMock.mockClass(DriverSQ);  
+        driver = new DriverSQ.default("111.222.333.444",1234);
+        communicator = new Communicator.default(driver);
+        commandBuilder = new CommandBuilderSQ.default();
+        commandBuilder.midiChannel = 2;
+        scene = new ModuleSQScenes(commandBuilder);
+    });
+
+    afterEach(function() {
+        communicatorMock.restore();
+        commandBuilderMock.restore();
+    });
+
+    it("setValue with true value", function() {
+        let setSceneStub = sinon.stub(scene, 'setScene');
+        setSceneStub.callsFake(function() {});
+
+        let value = new ValueState(true);
+
+        scene.setValue(3, value);
+
+        expect(setSceneStub.calledOnceWith(3)).to.be.true;
+    });
+
+    it("setValue with false value", function() {
+        let setSceneStub = sinon.stub(scene, 'setScene');
+        setSceneStub.callsFake(function() {});
+
+        let value = new ValueState(false);
+
+        scene.setValue(5, value);
+
+        expect(setSceneStub.notCalled).to.be.true;
+    });
+
+    it("setScene scene 96", function() {
+        let writeStub = communicatorMock.mock("write", "");
+        writeStub.callsFake(function(data: Uint8Array) {
+            expect(data[0], "byte1").to.be.eq(0xb2);
+            expect(data[1], "byte2").to.be.eq(0x00);
+            expect(data[2], "byte3").to.be.eq(0x00);
+            expect(data[3], "byte4").to.be.eq(0xc0);
+            expect(data[4], "byte5").to.be.eq(0x5f);
+        });
+
+        scene.setCommunicator(communicator);
+        scene.setScene(96);
+
+        expect(writeStub.calledOnce, "called communicator write").to.be.true;
+    });
+
+    it("setScene scene 264", function() {
+        let writeStub = communicatorMock.mock("write", "");
+        writeStub.callsFake(function(data: Uint8Array) {
+            expect(data[0], "byte1").to.be.eq(0xb2);
+            expect(data[1], "byte2").to.be.eq(0x00);
+            expect(data[2], "byte3").to.be.eq(0x02);
+            expect(data[3], "byte4").to.be.eq(0xc0);
+            expect(data[4], "byte5").to.be.eq(0x07);
+        });
+
+        scene.setCommunicator(communicator);
+        scene.setScene(264);
+
+        expect(writeStub.calledOnce, "called communicator write").to.be.true;
+    });
+
+    it("setScene throw no communicator", function() {
+        let reguestCall = sinon.spy(scene, "setScene");
+        try {
+            scene.setScene(3);
+          } catch (e) {
+              expect(e.message).to.be.eq("no communicator is set");
+          }
+        expect(reguestCall.threw()).to.be.true;        
+    });
+
+    it("setScene throw invalid scene", function() {
+        scene.setCommunicator(communicator);
+        let reguestCall = sinon.spy(scene, "setScene");
+        try {
+            scene.setScene(301);
+          } catch (e) {
+              expect(e.message).to.be.eq("wrong scene input");
+          }
+        expect(reguestCall.threw()).to.be.true;  
     });
 });
