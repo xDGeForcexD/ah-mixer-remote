b9c8e006234cad4e085a27df7d23c4ad9f4aadd2
xDGeForcexD <info@xdgeforcexd.eu>
2020-07-05 13:24:18 +0200

    softkey code added

--- lib/drivers/sq/modules/softkey.ts
+++ lib/drivers/sq/modules/softkey.ts
@@ -1,14 +1,45 @@
 import ModuleSoftkey from "../../../module/types/softkey";
-import IValue from "../../../types/structure/iValue";
+import ValueState from "../../../types/structure/valueState";
+
 
 class ModuleSQSoftkey extends ModuleSoftkey {
     driverRequiere: string = "sq";
 
-    setValue(channel: number, value: IValue): void {
-        throw new Error("Method not implemented.");
+    /**
+     * Takes softkey number and value to execute the softkey on the mixer
+     * @param channel Softkey number 1 - 16
+     * @param value ValueState Class with true / false
+     */
+    setValue(channel: number, value: ValueState): void {
+        if(this.communicator === null) {
+            throw new Error("no communicator is set");
+        }
+
+        if(channel < 1 || channel > 16) {
+            throw new Error("wrong softkey input");
+        }
+
+        let data = new Uint8Array(3);
+        if(value.value) {
+            data[0] = 0x90;
+            data[2] = 0x7f;
+        } else {
+            data[0] = 0x80;
+            data[2] = 0x00;
+        }
+        data[0] = data[0] + this.commandBuilder.midiChannel;
+        data[1] = 0x30 + channel - 1;
+
+        this.communicator.write(data);
     }
+    
+    /**
+     * Option not available!
+     * @param channel Scene number 1 - 300
+     * @ignore requestValue option is not available!
+     */
     requestValue(channel: number): void {
-        throw new Error("Method not implemented.");
+        throw new Error("option not available");
     }
 
 }
--- test/drivers/sq/modules/testSoftkey.ts
+++ test/drivers/sq/modules/testSoftkey.ts
@@ -1,7 +1,53 @@
 import { expect } from "chai";
+import { ImportMock, MockManager } from "ts-mock-imports";
+import * as Communicator from "../../../../lib/communicator/communicator";
+import * as CommandBuilderSQ from "../../../../lib/drivers/sq/commandBuilder";
+import * as DriverSQ from "../../../../lib/drivers/sq/driver";
+import ModuleSQSoftkey from "../../../../lib/drivers/sq/modules/softkey";
+import Driver from "../../../../lib/driver/driver";
+import ValueState from "../../../../lib/types/structure/valueState";
+
+const sinon = require("sinon");
 
 describe("TestModuleSQSoftkey", function() {
-    it("implement", function() {
-        expect(false).to.be.true;
+    let communicatorMock : MockManager<Communicator.default>;
+    let commandBuilderMock : MockManager<CommandBuilderSQ.default>;
+    let driverMock : MockManager<Driver>
+    let driver : Driver;
+    let communicator : Communicator.default;
+    let commandBuilder : CommandBuilderSQ.default;
+    let softkey : ModuleSQSoftkey;
+
+    const sandbox = sinon.createSandbox();
+    beforeEach(function() {
+        communicatorMock = ImportMock.mockClass(Communicator);
+        commandBuilderMock = ImportMock.mockClass(CommandBuilderSQ);
+        driverMock = ImportMock.mockClass(DriverSQ);  
+        driver = new DriverSQ.default("111.222.333.444",1234);
+        communicator = new Communicator.default(driver);
+        commandBuilder = new CommandBuilderSQ.default();
+        commandBuilder.midiChannel = 1;
+        softkey = new ModuleSQSoftkey(commandBuilder);
+    });
+
+    afterEach(function() {
+        communicatorMock.restore();
+        commandBuilderMock.restore();
+    });
+
+    it("setValue", function() {
+        let writeStub = communicatorMock.mock("write", "");
+        writeStub.callsFake(function(data: Uint8Array) {
+            expect(data[0], "byte1").to.be.eq(0x91);
+            expect(data[1], "byte2").to.be.eq(0x32);
+            expect(data[2], "byte3").to.be.eq(0x7f);
+        });
+
+        let value = new ValueState(true);
+
+        softkey.setCommunicator(communicator);
+        softkey.setValue(3, value);
+
+        expect(writeStub.calledOnce, "called communicator write").to.be.true;
     });
 });
