593b8366c62d04a5865af12cbac7c82e24cd354b
xDGeForcexD <info@xdgeforcexd.eu>
2020-07-05 13:30:18 +0200

    softkey test code added

--- test/drivers/sq/modules/testSoftkey.ts
+++ test/drivers/sq/modules/testSoftkey.ts
@@ -35,7 +35,7 @@ describe("TestModuleSQSoftkey", function() {
         commandBuilderMock.restore();
     });
 
-    it("setValue", function() {
+    it("setValue with true", function() {
         let writeStub = communicatorMock.mock("write", "");
         writeStub.callsFake(function(data: Uint8Array) {
             expect(data[0], "byte1").to.be.eq(0x91);
@@ -49,5 +49,42 @@ describe("TestModuleSQSoftkey", function() {
         softkey.setValue(3, value);
 
         expect(writeStub.calledOnce, "called communicator write").to.be.true;
+    });
+
+    it("setValue with false", function() {
+        let writeStub = communicatorMock.mock("write", "");
+        writeStub.callsFake(function(data: Uint8Array) {
+            expect(data[0], "byte1").to.be.eq(0x81);
+            expect(data[1], "byte2").to.be.eq(0x32);
+            expect(data[2], "byte3").to.be.eq(0x00);
+        });
+
+        let value = new ValueState(false);
+
+        softkey.setCommunicator(communicator);
+        softkey.setValue(3, value);
+
+        expect(writeStub.calledOnce, "called communicator write").to.be.true;
+    });
+
+    it("setValue throw no communicator", function() {
+        let reguestCall = sinon.spy(softkey, "setValue");
+        try {
+            softkey.setValue(3, new ValueState(true));
+          } catch (e) {
+              expect(e.message).to.be.eq("no communicator is set");
+          }
+        expect(reguestCall.threw()).to.be.true;        
+    });
+
+    it("setValue throw invalid softkey", function() {
+        softkey.setCommunicator(communicator);
+        let reguestCall = sinon.spy(softkey, "setValue");
+        try {
+            softkey.setValue(0, new ValueState(false));
+          } catch (e) {
+              expect(e.message).to.be.eq("wrong softkey input");
+          }
+        expect(reguestCall.threw()).to.be.true;  
     });
 });
